b1550cb577e34b9648ea7df9c87e6011de5d6b9f
Fernando Caldera <fernando@jobbatical.com>
2018-10-08 22:23:40 +0300

    Setup Koa server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-console.log("make the test fail");
+// server/index.js
+
+const Koa = require("koa");
+const articlesRoutes = require("./routes/articles.routes");
+
+const app = new Koa();
+const PORT = process.env.PORT || 8081;
+
+app.use(articlesRoutes.routes());
+
+const server = app.listen(PORT).on("error", err => {
+  console.error(err);
+});
+
+module.exports = server;
